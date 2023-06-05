545e68b559a8469b4e20af55d879488c117c9f06
Julen Etxaniz <juletxara@gmail.com>
2023-06-05 22:30:24 +0200

    add translation script for llama 65b

--- translate/translate_few_shot.py
+++ translate/translate_few_shot.py
@@ -99,7 +99,7 @@ def main(
 
     print(f"Loading model {model_name}...")
     model = AutoModelForCausalLM.from_pretrained(
-        pretrained_model_name_or_path=model_name, cache_dir=cache_dir
+        pretrained_model_name_or_path=model_name, cache_dir=cache_dir, device_map="auto"
     )
 
     model.eval()
